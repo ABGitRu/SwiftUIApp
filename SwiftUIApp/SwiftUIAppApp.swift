e65abc413ea1d84da5d5f385994b2cedfe2bf1e1
Alexey Barinov <begoodbebad2012@gmail.com>
2021-05-31 20:34:16 +0300

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Mac on 31.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
